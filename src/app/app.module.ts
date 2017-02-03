eb840ec5dec5f906db01bd16f819bf226accb410
Guenter Schafranek <gschafra@web.de>
2017-02-03 17:14:54 +0100

    Established firebase connection and first list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,17 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyAb9PDnIJwzOeJL5l6bGS3nJfBfHCJFg10",
+  authDomain: "project-5214608446069991723.firebaseapp.com",
+  databaseURL: "https://project-5214608446069991723.firebaseio.com",
+  storageBucket: "project-5214608446069991723.appspot.com",
+  messagingSenderId: "104701499060"
+};
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
