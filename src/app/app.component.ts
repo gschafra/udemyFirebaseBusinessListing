153f7a9864a1a01dd0dda88b9f3c41e80222ce42
Guenter Schafranek <gschafra@web.de>
2017-02-06 00:41:32 +0100

    Added firebase service and models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from './services/firebase.service';
+import { Category } from './models/Category';
+import { Business } from './models/Business';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FirebaseService]
 })
-export class AppComponent {
-  title = 'app works!';
-  items: FirebaseListObservable<any[]>;
-  constructor(af: AngularFire) {
-    console.log("blub");
-    this.items = af.database.list('/items');
-    console.log(this.items);
+export class AppComponent implements OnInit {
+  businesses: Business[];
+  categories: Category[];
+  constructor(private _firebaseService: FirebaseService) {
+  }
+
+  ngOnInit() {
+    this._firebaseService.getBusinesses().subscribe(
+      businesses => {
+        this.businesses = businesses;
+      }
+    );
+    this._firebaseService.getCategories().subscribe(
+      categories => {
+        this.categories = categories;
+      }
+    );
   }
 }
