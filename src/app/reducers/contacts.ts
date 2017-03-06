da8a28c67cf8c8cc65d82e2bff4c335067bf7c89
Guenter Schafranek <gschafra@web.de>
2017-03-06 23:35:59 +0100

    Using arrow functions for reducers

--- src/app/reducers/contacts.ts
+++ src/app/reducers/contacts.ts
@@ -3,12 +3,18 @@ import { Business } from '../models/Business';
 
 export const ADD_CONTACTS = 'ADD_CONTACTS';
 export const REMOVE_CONTACT = 'REMOVE_CONTACT';
+export const ADD_CONTACT = 'ADD_CONTACT';
 
-export function contactsReducer(state: Business[] = [], action: Action) {
+export const contactsReducer = (state: Business[] = [], action: Action) => {
     console.log('contactsReducer');
     switch (action.type) {
         case ADD_CONTACTS:
-            return action.payload;
+            return [...action.payload];
+        case ADD_CONTACT:
+            return [
+                ...state,
+                action.payload
+            ];
         case REMOVE_CONTACT:
             break;
         default:
--- src/app/reducers/selectedContact.ts
+++ src/app/reducers/selectedContact.ts
@@ -2,7 +2,7 @@ import { ActionReducer, Action } from '@ngrx/store';
 
 export const EDIT = 'EDIT';
 
-export function selectedContactReducer(state: any = null, action: Action) {
+export const selectedContactReducer = (state: any = null, action: Action) => {
   switch (action.type) {
     case EDIT:
         break;
